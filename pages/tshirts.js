5185f899e829ab4abff603bd93a3e117727c0091
AchuthKumarTelugu <achuthkumar.telugu123@gmail.com>
2024-04-15 21:21:23 +0530

    Link

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,169 +1,191 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 const Tshirts = () => {
   return (
     <div>
       <section className="text-gray-600 body-font">
         <div className="container px-14 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Custom-Printed-T-Shirt-Round-Neck.jpg"
-                  width={400} height={600}
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The Catalyzer
-                </h2>
-                <p className="mt-1">₹16.00</p>
-                <p className='mt-1'>S M L XL XXL</p>
+            
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer">
+                <a className="block relative h-48 rounded overflow-hidden">
+                <Link href={'products/wear-the-code'}>
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Custom-Printed-T-Shirt-Round-Neck.jpg"
+                    width={400} height={600}
+                  />
+                   </Link>
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">₹16.00</p>
+                  <p className='mt-1'>S M L XL XXL</p>
+                </div>
               </div>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src={`https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Custom-Printed-T-Shirt-Round-Neck.jpg`}
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  Shooting Stars
-                </h2>
-                <p className="mt-1">₹21.15</p>
-                <p className='mt-1'>S M L XL XXL</p>
+           
+            
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer">
+                <a className="block relative h-48 rounded overflow-hidden">
+                <Link href={'products/wear-the-code'}>
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src={`https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Custom-Printed-T-Shirt-Round-Neck.jpg`}
+                  />
+                  </Link>
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    Shooting Stars
+                  </h2>
+                  <p className="mt-1">₹21.15</p>
+                  <p className='mt-1'>S M L XL XXL</p>
+                </div>
               </div>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Custom-Printed-T-Shirt-Round-Neck.jpg"
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  Neptune
-                </h2>
-                <p className="mt-1">₹12.00</p>
-                <p className='mt-1'>S M L XL XXL</p>
+            
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer">
+                <a className="block relative h-48 rounded overflow-hidden">
+                <Link href={'products/wear-the-code'}>
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Custom-Printed-T-Shirt-Round-Neck.jpg"
+                  />
+                  </Link>
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    Neptune
+                  </h2>
+                  <p className="mt-1">₹12.00</p>
+                  <p className='mt-1'>S M L XL XXL</p>
+                </div>
               </div>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Custom-Printed-T-Shirt-Round-Neck.jpg"
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The 400 Blows
-                </h2>
-                <p className="mt-1">₹18.40</p>
-                <p className='mt-1'>S M L XL XXL</p>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer">
+                <a className="block relative h-48 rounded overflow-hidden">
+                <Link href={'products/wear-the-code'}>
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Custom-Printed-T-Shirt-Round-Neck.jpg"
+                  />
+                  </Link>
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The 400 Blows
+                  </h2>
+                  <p className="mt-1">₹18.40</p>
+                  <p className='mt-1'>S M L XL XXL</p>
+                </div>
               </div>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Custom-Printed-T-Shirt-Round-Neck.jpg"
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The Catalyzer
-                </h2>
-                <p className="mt-1">₹16.00</p>
-                <p className='mt-1'>S M L XL XXL</p>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer">
+                <a className="block relative h-48 rounded overflow-hidden">
+                <Link href={'products/wear-the-code'}>
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Custom-Printed-T-Shirt-Round-Neck.jpg"
+                  />
+                  </Link>
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The Catalyzer
+                  </h2>
+                  <p className="mt-1">₹16.00</p>
+                  <p className='mt-1'>S M L XL XXL</p>
+                </div>
               </div>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Custom-Printed-T-Shirt-Round-Neck.jpg"
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  Shooting Stars
-                </h2>
-                <p className="mt-1">₹21.15</p>
-                <p className='mt-1'>S M L XL XXL</p>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer">
+                <a className="block relative h-48 rounded overflow-hidden">
+                <Link href={'products/wear-the-code'}>
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Custom-Printed-T-Shirt-Round-Neck.jpg"
+                  />
+                  </Link>
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    Shooting Stars
+                  </h2>
+                  <p className="mt-1">₹21.15</p>
+                  <p className='mt-1'>S M L XL XXL</p>
+                </div>
               </div>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Custom-Printed-T-Shirt-Round-Neck.jpg"
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  Neptune
-                </h2>
-                <p className="mt-1">₹12.00</p>
-                <p className='mt-1'>S M L XL XXL</p>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer">
+                <a className="block relative h-48 rounded overflow-hidden">
+                <Link href={'products/wear-the-code'}>
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Custom-Printed-T-Shirt-Round-Neck.jpg"
+                  />
+                  </Link>
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    Neptune
+                  </h2>
+                  <p className="mt-1">₹12.00</p>
+                  <p className='mt-1'>S M L XL XXL</p>
+                </div>
               </div>
-            </div>
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-              <a className="block relative h-48 rounded overflow-hidden">
-                <img
-                  alt="ecommerce"
-                  className="object-cover object-center w-full h-full block"
-                  src="https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Custom-Printed-T-Shirt-Round-Neck.jpg"
-                />
-              </a>
-              <div className="mt-4">
-                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                  CATEGORY
-                </h3>
-                <h2 className="text-gray-900 title-font text-lg font-medium">
-                  The 400 Blows
-                </h2>
-                <p className="mt-1">₹18.40</p>
-                <p className='mt-1'>S M L XL XXL</p>
+              <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer">
+                <a className="block relative h-48 rounded overflow-hidden">
+                <Link href={'products/wear-the-code'}>
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://d1xv5jidmf7h0f.cloudfront.net/circleone/images/products_gallery_images/Custom-Printed-T-Shirt-Round-Neck.jpg"
+                  />
+                  </Link>
+                </a>
+                <div className="mt-4">
+                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                    CATEGORY
+                  </h3>
+                  <h2 className="text-gray-900 title-font text-lg font-medium">
+                    The 400 Blows
+                  </h2>
+                  <p className="mt-1">₹18.40</p>
+                  <p className='mt-1'>S M L XL XXL</p>
+                </div>
               </div>
-            </div>
           </div>
-        </div>
-      </section>
 
-    </div>
+        </div>
+      </section >
+
+    </div >
   )
 }
 
