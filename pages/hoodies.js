f7d3f421869a912b48809a89e9c1c8ec04907f40
AchuthKumarTelugu <achuthkumar.telugu123@gmail.com>
2024-04-15 22:20:28 +0530

    edited mugs,stickers,hoodies,tshirts page similar way

--- pages/hoodies.js
+++ pages/hoodies.js
@@ -1,11 +1,203 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const Hoodies = () => {
   return (
     <div>
-      this is hoodies page
+      <section className="text-gray-600 body-font">
+        <div className="container px-14 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4 justify-center">
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
+              <Link href={'products/wear-the-code'}>
+              <div className="block relative h-48 rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://assets.ajio.com/medias/sys_master/root/20230919/B4yz/65094b2fddf7791519e6ff00/-473Wx593H-466597686-black-MODEL.jpg"
+                    width={400}
+                    height={600}
+                  />
+                  </div>
+              </Link>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The Catalyzer
+                </h2>
+                <p className="mt-1">₹16.00</p>
+                <p className='mt-1'>S M L XL XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
+              <Link href={'products/wear-the-code'}>
+              <div className="block relative h-48 rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://assets.ajio.com/medias/sys_master/root/20230919/B4yz/65094b2fddf7791519e6ff00/-473Wx593H-466597686-black-MODEL.jpg"
+                    width={400}
+                    height={600}
+                  />
+                  </div>
+              </Link>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The Catalyzer
+                </h2>
+                <p className="mt-1">₹16.00</p>
+                <p className='mt-1'>S M L XL XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
+              <Link href={'products/wear-the-code'}>
+              <div className="block relative h-48 rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://assets.ajio.com/medias/sys_master/root/20230919/B4yz/65094b2fddf7791519e6ff00/-473Wx593H-466597686-black-MODEL.jpg"
+                    width={400}
+                    height={600}
+                  />
+                  </div>
+              </Link>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The Catalyzer
+                </h2>
+                <p className="mt-1">₹16.00</p>
+                <p className='mt-1'>S M L XL XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
+              <Link href={'products/wear-the-code'}>
+              <div className="block relative h-48 rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://assets.ajio.com/medias/sys_master/root/20230919/B4yz/65094b2fddf7791519e6ff00/-473Wx593H-466597686-black-MODEL.jpg"
+                    width={400}
+                    height={600}
+                  />
+                  </div>
+              </Link>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The Catalyzer
+                </h2>
+                <p className="mt-1">₹16.00</p>
+                <p className='mt-1'>S M L XL XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
+              <Link href={'products/wear-the-code'}>
+              <div className="block relative h-48 rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://assets.ajio.com/medias/sys_master/root/20230919/B4yz/65094b2fddf7791519e6ff00/-473Wx593H-466597686-black-MODEL.jpg"
+                    width={400}
+                    height={600}
+                  />
+                  </div>
+              </Link>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The Catalyzer
+                </h2>
+                <p className="mt-1">₹16.00</p>
+                <p className='mt-1'>S M L XL XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
+              <Link href={'products/wear-the-code'}>
+              <div className="block relative h-48 rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://assets.ajio.com/medias/sys_master/root/20230919/B4yz/65094b2fddf7791519e6ff00/-473Wx593H-466597686-black-MODEL.jpg"
+                    width={400}
+                    height={600}
+                  />
+                  </div>
+              </Link>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The Catalyzer
+                </h2>
+                <p className="mt-1">₹16.00</p>
+                <p className='mt-1'>S M L XL XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
+              <Link href={'products/wear-the-code'}>
+              <div className="block relative h-48 rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://assets.ajio.com/medias/sys_master/root/20230919/B4yz/65094b2fddf7791519e6ff00/-473Wx593H-466597686-black-MODEL.jpg"
+                    width={400}
+                    height={600}
+                  />
+                  </div>
+              </Link>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The Catalyzer
+                </h2>
+                <p className="mt-1">₹16.00</p>
+                <p className='mt-1'>S M L XL XXL</p>
+              </div>
+            </div>
+            <div className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-2">
+              <Link href={'products/wear-the-code'}>
+              <div className="block relative h-48 rounded overflow-hidden">
+                  <img
+                    alt="ecommerce"
+                    className="object-cover object-center w-full h-full block"
+                    src="https://assets.ajio.com/medias/sys_master/root/20230919/B4yz/65094b2fddf7791519e6ff00/-473Wx593H-466597686-black-MODEL.jpg"
+                    width={400}
+                    height={600}
+                  />
+                  </div>
+              </Link>
+              <div className="mt-4">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                  CATEGORY
+                </h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">
+                  The Catalyzer
+                </h2>
+                <p className="mt-1">₹16.00</p>
+                <p className='mt-1'>S M L XL XXL</p>
+              </div>
+            </div>
+          
+            {/* Repeat similar structure for other T-shirts */}
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default Hoodies
+export default Hoodies;
